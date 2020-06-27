1c83a69baa9b3f54c6a92c25384788fdcc7c408a
nough <nough@noughs-MacBook-Pro.local>
2020-06-28 03:34:09 +0900

    first commit

---
+++ src/posts/tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from django.test import TestCase
+
+# Create your tests here.
+class TestPostsTest(TestCase):
+
+    def test_smoke_test(self):
+        assert 1 is not 1, "당연히 같아야 해요."
+        
+    

1f803d11359d4848cb1dea8b03d89027cea2a236
nough <nough@noughs-MacBook-Pro.local>
2020-06-28 14:47:16 +0900

    Add Some Tests

--- scripts/pytests.sh
+++ scripts/pytests.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# cd ../src && python3 manage.py test --settings=superapi.settings.developments
 
-cd ../src && python3 manage.py test --settings=superapi.settings.developments
+# cd ../src && pytest -s posts/tests/tests_all.py
+
+cd ../src && pytest -s posts/tests/test_models.py
+

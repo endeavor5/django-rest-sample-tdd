6d24a70da6ee1b1af7caaa2cba3d77920be3c559
nough <nough@noughs-MacBook-Pro.local>
2020-06-28 19:31:35 +0900

    Add Some Tests 002

--- scripts/pytests.sh
+++ scripts/pytests.sh
@@ -4,5 +4,11 @@
 
 # cd ../src && pytest -s posts/tests/tests_all.py
 
-cd ../src && pytest -s posts/tests/test_models.py
+# cd ../src && pytest -s posts/tests/test_models.py
 
+# -x --ipdb 는 첫 실패시에 그 다음 코드에 브레이크 포인터를 걸어버린다
+# pytest-ipdb repo https://github.com/mverteuil/pytest-ipdb
+
+# cd ../src && pytest -s posts/tests/test_models.py -x --ipdb
+
+cd ../src && pytest -s posts/tests/test_views.py
